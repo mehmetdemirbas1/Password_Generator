0a4119e82698f2f0adf5c0c0e95e935e5defa902
Mehmet Demirbas <mehmet1demirbas@gmail.com>
2025-12-26 14:36:35 +0300

    generate_passwd fonction created

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <windows.h>
+#include <bcrypt.h>
+#pragma comment(lib, "bcrypt.lib")
 
-void generate_passwd(void* memory, int lenght)
+void generate_passwd(char* memory, int length)
 {
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+	size_t charset_len = sizeof(charset) - 1;
+	NTSTATUS status = BCryptGenRandom(
+		NULL,
+		(PUCHAR)memory,
+		(ULONG)length,
+		BCRYPT_USE_SYSTEM_PREFERRED_RNG
+	);
 
+	if (!BCRYPT_SUCCESS(status)) {
+		return;
+	}
+
+	for (int i = 0; i < length; ++i) {
+		unsigned char random_byte = (unsigned char)*(memory + i);
+		*(memory + i) = charset[random_byte % charset_len];
+	}
 }
 
 int main(int argc, const char* argv[])
@@ -11,13 +30,12 @@ int main(int argc, const char* argv[])
 	if (argc != 2) {
 		printf("Usage: passgen <number of the bytes>\n");
 	}
-	int lenght = atoi(argv[1]);
-	printf("Generating password of %d bytes\n", lenght);
+	int length = atoi(argv[1]);
+	printf("Generating password of %d bytes\n", length);
 
-	void* password = malloc(lenght);
-
-
-
+	char* password = (char*)malloc(length);
+	generate_passwd(password, length);
+	printf("Genereted Password: %s \n", password);
 
 	free(password);
 }
