e62f6de87081f00fb4f62cdb628d39410cb3e890
Mehmet Demirbas <mehmet1demirbas@gmail.com>
2025-12-26 13:52:38 +0300

    including stdlib.h argumant controlling

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int main()
+void generate_passwd(void* memory, int lenght)
 {
 
+}
+
+int main(int argc, const char* argv[])
+{
+	if (argc != 2) {
+		printf("Usage: passgen <number of the bytes>\n");
+	}
+	int lenght = atoi(argv[1]);
+	printf("Generating password of %d bytes\n", lenght);
+
+	void* password = malloc(lenght);
+
+
+
+
+	free(password);
 }
